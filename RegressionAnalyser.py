38b66f2b8b76c3ba707fc1c5a1e619ccae30ddc7
neevan0842 <manojnaveen085@gmail.com>
2024-03-16 14:26:13 +0530

    updated to fit Housing Data

--- RegressionAnalyser.py
+++ RegressionAnalyser.py
@@ -3,31 +3,38 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class RegressionAnalyser:
-    def __init__(self, path, missing_data=None, categorical_data=None, label_encoding=False):
+    def __init__(self, path, res=None, missing_data=[], categorical_data=[], label_encoding=[]):
         self.dataset = pd.read_csv(path)
         self.result = dict()
-        self.X = self.dataset.iloc[:, :-1].values
-        self.y = self.dataset.iloc[:, -1].values
+        if res is None:
+            res = len(self.dataset.columns) - 1
+        self.X = self.dataset.iloc[:, [i for i in range(len(self.dataset.columns)) if i != res]].values
+        self.y = self.dataset.iloc[:, res].values
+
+        #updated categorical_data and label_encoding
+        categorical_data = [i - 1 if i > res else i for i in categorical_data]
+        label_encoding = [i - 1 if i > res else i for i in label_encoding]
 
         # Handling Missing Data
-        if missing_data is not None:
+        if missing_data:
             from sklearn.impute import SimpleImputer
             imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
             imputer.fit(self.X[:, missing_data])
             self.X[:, missing_data] = imputer.transform(self.X[:, missing_data])
 
-        # Category Data Encoding
-        if categorical_data is not None:
-            from sklearn.compose import ColumnTransformer
-            from sklearn.preprocessing import OneHotEncoder
-            ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_data)], remainder='passthrough')
-            self.X = np.array(ct.fit_transform(self.X))
-
         # Label Encoding
         if label_encoding:
             from sklearn.preprocessing import LabelEncoder
             le = LabelEncoder()
-            self.y = le.fit_transform(self.y)
+            for i in label_encoding:
+                self.X[:, [i]] = le.fit_transform(self.X[:, [i]].reshape(-1)).reshape(-1,1)
+
+        # Category Data Encoding
+        if categorical_data:
+            from sklearn.compose import ColumnTransformer
+            from sklearn.preprocessing import OneHotEncoder
+            ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_data)], remainder='passthrough')
+            self.X = np.array(ct.fit_transform(self.X))
 
         # Feature Scaling
         from sklearn.preprocessing import StandardScaler
@@ -134,9 +141,8 @@ class RegressionAnalyser:
 
         # Define parameter grid for grid search
         support_vector_regression_parameters = {
-            'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+            'kernel': ['linear', 'poly', 'rbf'],
             'degree': [2, 3, 4, 5],
-            'gamma': ['scale', 'auto']
         }
 
         # Perform grid search to find the best parameters
@@ -170,7 +176,6 @@ class RegressionAnalyser:
         # Define parameter grid for grid search
         decision_tree_regression_parameters = {
             'criterion': ['poisson', 'absolute_error', 'friedman_mse', 'squared_error'],
-            'splitter': ['best', 'random']
         }
 
         # Perform grid search to find the best parameters
@@ -203,7 +208,6 @@ class RegressionAnalyser:
 
         # Define parameter grid for grid search
         random_forest_regression_parameters = {
-            'n_estimators': [10, 100, 1000],
             'criterion': ['poisson', 'absolute_error', 'friedman_mse', 'squared_error']  # Update the criterion parameter
         }
 
@@ -238,7 +242,6 @@ class RegressionAnalyser:
 
         # Define parameter grid for grid search
         xgboost_parameters = {
-            'n_estimators': [100, 500, 1000],
             'booster': ['gbtree', 'gblinear', 'dart'],
             'learning_rate': [0.01, 0.1, 0.3]
         }
@@ -248,23 +251,23 @@ class RegressionAnalyser:
 
         # Reassign the model instance with the best parameters found
         model = XGBRegressor(**best_params)
-        
+
         # Perform k-fold cross-validation with the tuned model
         mean_score, std_score = self.k_fold_cross_validation(model)
-        
+
         # Store the result
         self.result['XGBoost'] = (mean_score, std_score, best_params)
-        
+
         # Print the results
         print('XGBoost:')
         print('Mean R-squared Score:', mean_score)
         print('Standard Deviation of R-squared Score:', std_score)
         print('Best Parameters:', best_params)
-        
+
         # Return the best parameters for further analysis if needed
         return best_params
 
-    
+
     def CatBoost(self):
         from catboost import CatBoostRegressor
 
@@ -273,7 +276,6 @@ class RegressionAnalyser:
 
         # Define parameter grid for grid search
         catboost_parameters = {
-            'iterations': [100, 500, 1000],
             'learning_rate': [0.01, 0.1, 0.3]
         }
 
@@ -282,23 +284,23 @@ class RegressionAnalyser:
 
         # Reassign the model instance with the best parameters found
         model = CatBoostRegressor(**best_params)
-        
+
         # Perform k-fold cross-validation with the tuned model
         mean_score, std_score = self.k_fold_cross_validation(model)
-        
+
         # Store the result
         self.result['CatBoost'] = (mean_score, std_score , best_params)
-        
+
         # Print the results
         print('CatBoost:')
         print('Mean R-squared Score:', mean_score)
         print('Standard Deviation of R-squared Score:', std_score)
         print('Best Parameters:', best_params)
-        
+
         # Return the best parameters for further analysis if needed
         return best_params
-  
-    
+
+
     def analyse(self):
         self.linear_regression()
         self.polynomial_regression()
@@ -309,14 +311,13 @@ class RegressionAnalyser:
         self.CatBoost()
         from pprint import pprint
         pprint(self.result)
-        
-    
+
+
     def plot(self):
         for key,value in self.result.items():
             plt.scatter(value[0], value[1], label=key)
         plt.title('R-squared Score vs Standard Deviation')
         plt.xlabel('Mean R-squared Score')
-        plt.ylabel('Standard Deviation of R-squared Score') 
+        plt.ylabel('Standard Deviation of R-squared Score')
         plt.legend()
-        plt.show()
-        
+        plt.savefig('result.png')
